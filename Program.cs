4550f423f2ed0d43ec79c66052ae630c3fccf0a9
unknown <brodny@wp.pl>
2020-10-22 20:48:31 +0200

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sprache_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
