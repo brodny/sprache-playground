4db9f80639fe9c3c92b05c500afef756bb7e79b5
unknown <brodny@wp.pl>
2020-10-22 21:56:34 +0200

    ExpressionParser fields reordered, NullReferenceException in runtime

--- ExpressionParser.cs
+++ ExpressionParser.cs
@@ -41,25 +41,14 @@ namespace SprachePlayground
         private static readonly Parser<ExpressionType> Modulo = Operator("%", ExpressionType.Modulo);
         private static readonly Parser<ExpressionType> Power = Operator("^", ExpressionType.Power);
 
-        private static readonly Parser<Expression> Function =
-            from name in Parse.Letter.AtLeastOnce().Text()
-            from lParenthesis in Parse.Char('(')
-            from expr in Parse.Ref(() => Expr).DelimitedBy(Parse.Char(',').Token())
-            from rParenthesis in Parse.Char(')')
-            select CallFunction(name, expr.ToArray());
-        
-        private static readonly Parser<Expression> Constant =
-            Parse.Decimal.Select(x => Expression.Constant(double.Parse(x))).Named("number");
-        
-        private static readonly Parser<Expression> Factor =
-            (
-                from lParenthesis in Parse.Char('(')
-                from expr in Parse.Ref(() => Expr)
-                from rParenthesis in Parse.Char(')')
-                select expr
-            ).Named("expression")
-            .XOr(Constant)
-            .XOr(Function);
+        private static readonly Parser<Expression> Expr =
+            Parse.ChainOperator(Add.Or(Subtract), Term, Expression.MakeBinary);
+
+        private static readonly Parser<Expression> Term =
+            Parse.ChainOperator(Multiply.Or(Divide).Or(Modulo), InnerTerm, Expression.MakeBinary);
+
+        private static readonly Parser<Expression> InnerTerm =
+            Parse.ChainRightOperator(Power, Operand, Expression.MakeBinary);
 
         private static readonly Parser<Expression> Operand =
             (
@@ -70,15 +59,26 @@ namespace SprachePlayground
                 )
                 .XOr(Factor)
             ).Token();
-
-        private static readonly Parser<Expression> InnerTerm =
-            Parse.ChainRightOperator(Power, Operand, Expression.MakeBinary);
-
-        private static readonly Parser<Expression> Term =
-            Parse.ChainOperator(Multiply.Or(Divide).Or(Modulo), InnerTerm, Expression.MakeBinary);
         
-        private static readonly Parser<Expression> Expr =
-            Parse.ChainOperator(Add.Or(Subtract), Term, Expression.MakeBinary);
+        private static readonly Parser<Expression> Factor =
+            (
+                from lParenthesis in Parse.Char('(')
+                from expr in Parse.Ref(() => Expr)
+                from rParenthesis in Parse.Char(')')
+                select expr
+            ).Named("expression")
+            .XOr(Constant)
+            .XOr(Function);
+        
+        private static readonly Parser<Expression> Constant =
+            Parse.Decimal.Select(x => Expression.Constant(double.Parse(x))).Named("number");
+        
+        private static readonly Parser<Expression> Function =
+            from name in Parse.Letter.AtLeastOnce().Text()
+            from lParenthesis in Parse.Char('(')
+            from expr in Parse.Ref(() => Expr).DelimitedBy(Parse.Char(',').Token())
+            from rParenthesis in Parse.Char(')')
+            select CallFunction(name, expr.ToArray());
 
         private static readonly Parser<Expression<Func<double>>> Lambda =
             Expr.End().Select(body => Expression.Lambda<Func<double>>(body));
